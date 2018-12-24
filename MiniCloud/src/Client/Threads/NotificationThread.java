7305eaf88bec0b77f0be8449e26c7fce529f0dba
Wallace Oliveira <wallace9655@gmail.com>
2018-12-24 18:15:38 +0000

    NotificationThread working partially for testing

--- MiniCloud/src/Client/Threads/NotificationThread.java
+++ MiniCloud/src/Client/Threads/NotificationThread.java
@@ -1,23 +1,49 @@
 package Client.Threads;
 
 import Client.DataObservable;
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NotificationThread extends Thread {
+    private ServerSocket server;
     private Socket socket;
-    DataObservable observable;
+    private DataObservable observable;
     
-    public NotificationThread(DataObservable obs){
-        this.observable = obs;
+    private boolean CONTINUE;
+    
+    public NotificationThread(DataObservable obs) throws IOException{
+            this.observable = obs;
+            CONTINUE = true;
+            
+            server = new ServerSocket(0,5);
+    }
+    
+    public void exit(){
+        try {
+            server.close();
+        } catch (IOException ex) {
+            System.out.println("Socket can't close!");
+        }
         
-        //TODO: Create server socket and other great things
+        CONTINUE = false;
     }
     
     public int getPort(){
-        return socket.getLocalPort();
+        return server.getLocalPort();
     }
     
     @Override
     public void run(){
+        try {
+            socket = server.accept();
+            
+            //while(CONTINUE){
+            //}
+        } catch (IOException ex) {
+            throw new RuntimeException("Error accepting the socket");
+        }
     }
 }
